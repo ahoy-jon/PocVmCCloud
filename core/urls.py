59fe535f0c44c6f6902f2a5158adda6e8a0e3328
mycloud <mycloud@debian.localdomain>
2012-01-21 11:11:24 +0100

    change configurations for easier deployment

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('core.views',
+urlpatterns = patterns('gate.core.views',
     url(r'^$', 'home'),
     url(r'^tests/$', 'home_tests'),
 )
